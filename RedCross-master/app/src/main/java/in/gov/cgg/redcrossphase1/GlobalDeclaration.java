14c3077a403ea621cc8ca8c6282fe289c37fe6b3
Tejasvi.b <Tejasvi.b@cgg.gov.in>
2019-11-20 16:11:25 +0530

    test

--- RedCross-master/app/src/main/java/in/gov/cgg/redcrossphase1/GlobalDeclaration.java
+++ RedCross-master/app/src/main/java/in/gov/cgg/redcrossphase1/GlobalDeclaration.java
@@ -9,7 +9,7 @@ public class GlobalDeclaration {
     public static String role = "";
     public static DashboardCountResponse counts;
     public static String username = "";
-    public static String sharedUname;
-    public static String sharedUPswd;
+    public static String sharedUname = "";
+    public static String sharedUPswd = "";
     // public static String BASE_URL="http://qa2.cgg.gov.in:8081/redcross/";
 }
